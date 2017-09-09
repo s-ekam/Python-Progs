3528466f21dc615dfc2249a3d89c0e9dbfdecc6a
Deep Kiran <deepkiran97.artist@gmail.com>
2017-09-09 23:07:27 +0530

    Create geneticAlgorithm.py

---
+++ geneticAlgorithm.py
@@ -0,0 +1,21 @@
+
+import random
+
+Popsize =10
+pop =[]        # store population with fitness
+D =5           #problem dimension
+LB =-30
+UB = 30
+class Individual:
+    def _init_(self, C,F):
+        self.Chromosome =C
+        self.Fitness =F
+def Init():
+    for i in range (0,Popsize):
+        Chromosome =[round(random.uniform(LB,UB),2) for j in range(0,D)]
+        Fitness =FitnessFunction(Chromosome)
+        NewIndividual =Individual(Chromosome,Fitness)
+        pop.append(NewIndividual)
+def FitnessFunction(x):
+    s = (x[4]-x[3]**2)**2+(x[3]-x[2]**2)**2+(x[3]-x[2]**2)**2+(x[2]-x[1]**2)**2+(x[1]-x[0]**2)**2
+    return round(s,2)
